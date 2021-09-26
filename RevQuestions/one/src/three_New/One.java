c144edf71ac2f3b4a65a0e7230c49ad3091464e0
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2021-09-26 20:48:27 +0530

    Add files via upload

--- RevQuestions/one/src/three_New/One.java
+++ RevQuestions/one/src/three_New/One.java
@@ -1,9 +1,14 @@
 package three_New;
 
+import java.util.Arrays;
+
 public class One {
 
     public static void main(String[] args) {
 
+        System.out.println(Integer.toBinaryString(12));
+
+        System.out.println(12 & (-12));
 
     }
 
@@ -57,9 +62,9 @@ public class One {
 
     //   Number of digits in binary
 
-    public static int numOfDigitsInBinary(int n){
+    public static int numOfDigitsInBinary(int n) {
 
-        return (int)(Math.log10(n)/Math.log10(2)) + 1 ;
+        return (int) (Math.log10(n) / Math.log10(2)) + 1;
 
     }
 
@@ -78,23 +83,120 @@ public class One {
 //             1  6  15  20  15  6  1
 
 
-    public static int sumOfNthRow(int  n) {
+    public static int sumOfNthRow(int n) {
 
-        return 1 << (n-1);
+        return 1 << (n - 1);
 
     }
 
 
     // ---------------------==================------------------------====================-----------------=================-------------
 
-  ///     finding if the number is in power of two or not
+    ///     finding if the number is in power of two or not
 
-    public static boolean  isInPowerOfTwo (int n){
-
-        if ( (n & (n-1)) == 0 ){return true;}
-        else return false;
+    public static boolean isInPowerOfTwo(int n) {
+        if (n == 0) {
+            return false;
+        } else if ((n & (n - 1)) == 0) {
+            return true;
+        } else return false;
 
     }
+
+    // ---------------------==================------------------------====================-----------------=================-------------
+
+    // Finding no. of set bits
+
+    public static int noOfSetBits(int n) {
+
+        int count = 0;
+
+        while (n < 0) {
+            count++;
+            n = n - (n & (-n));
+
+        }
+
+        return count;
+    }
+
+    // ---------------------==================------------------------====================-----------------=================-------------
+
+    // xOR from  a to b ;
+
+    public static int xOR(int a) {
+
+        if (a % 4 == 0) {
+            return a;
+        } else if (a % 4 == 1) {
+            return 1;
+        } else if (a % 4 == 2) {
+            return a + 1;
+        } else {
+            return 0;
+        }
+
+    }
+
+    public static int xORInRange(int a, int b) {
+
+        return xOR(b) ^ xOR(a - 1);
+
+    }
+
+
+    // ---------------------==================------------------------====================-----------------=================-------------
+
+
+    // https://leetcode.com/problems/flipping-an-image/
+
+    public static int[][] flippingAnImage(int arr[][]) {
+
+
+        for (int[] i : arr) {
+
+            flipTheArray(i);
+            for (int j = 0; j < i.length; j++) {
+                i[j] = i[j] ^ 1;
+            }
+
+        }
+
+        return arr;
+    }
+
+    private static void flipTheArray(int[] i) {
+
+        int start = 0;
+        int end = i.length - 1;
+
+        while (start <= end) {
+
+            int temp = i[start];
+            i[start] = i[end];
+            i[end] = temp;
+
+            start++;
+            end--;
+//            System.out.println("flip");
+//            System.out.println(Arrays.toString(i));
+
+        }
+
+
+    }
+
+
+
+    // ---------------------==================------------------------====================-----------------=================-------------
+
+
+
+
+
+
+
+
 
 
 }
--- RevQuestions/one/src/two_Test/One_Test.java
+++ RevQuestions/one/src/two_Test/One_Test.java
@@ -7,22 +7,50 @@ public class One_Test {
 
     public static void main(String[] args) {
 
+        int arr [] [] =  {{1,1,0},{1,0,1},{0,0,0}};
 
-       // int arr[] = {5,1,3};
-         int arr [] = {2,3,3,4,2,6,4};
+        System.out.println(Arrays.toString(flippingAnImage(arr)));
 
-        System.out.println(ans1(arr));
     }
 
 
 
-    public static int ans1(int arr[]) {
+    public static int[][] flippingAnImage(int arr [][] ) {
 
-        int unique = 0;
-        for (int n : arr) {
-            unique ^= n;
+
+        for (int [] i : arr){
+
+            flipTheArray(i);
+            System.out.println(" We are in flipping an image");
+            System.out.println(Arrays.toString(i));
+            for (int j = 0 ; j < i.length ; j++){
+                i[j] = i[j]^1;
+            }
+            System.out.println(" after reversing the bit ");
+            System.out.println(Arrays.toString(i));
         }
-        return unique;
+
+        return arr;
+    }
+
+    private static void flipTheArray(int[] i) {
+
+        int start =  0 ;
+        int end = i.length - 1;
+
+        while (start<=end){
+
+            int temp = i[start];
+            i[start]= i[end];
+            i[end] =temp;
+
+            start++;
+            end--;
+//            System.out.println("flip");
+//            System.out.println(Arrays.toString(i));
+
+        }
+
     }
 
 
